fe56a85fba99747a0e43b80fb1b0bb5d9e56be73
theprithwisingh <prithwisingh77@gmail.com>
2024-07-17 23:21:13 +0530

    update

--- Backend/index.js
+++ Backend/index.js
@@ -8,40 +8,38 @@ const path = require("path");
 const cors = require("cors");
 
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 
 //Database Connection With MongoDB
 mongoose.connect("mongodb+srv://prithwisingh77:Prithwi@139@cluster0.nvv9rdw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
 //API Creation
 app.get("/",(req,res)=>{
-    res.send("Express app is runnig");
+    res.send("Express app is running");
 })
 
 //Image storage Engine
 const storage  = multer.diskStorage({
     destination:'./upload/images',
-    filename:(req,file,cd)=>{
-        return cd(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    filename:(req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
     }
-})
-const upload = multer({storage:storage})
+});
+const upload = multer({ storage: storage });
 //creating Upload endpoint for images
-app.use("/image" ,express.static('upload/images'));
+app.use("/image", express.static('upload/images'));
 
-app.post("/upload", upload.single('product'),(req,res)=>{
+app.post("/upload", upload.single('product'), (req, res) => {
     res.json({
-      sucess:1,
-     image_url:`http://localhost:${port}/images/${req.file.filename}`
-})
-     
-})
+        success: 1,
+        image_url: `http://localhost:${port}/image/${req.file.filename}`
+    });
+});
 
-app.listen(port,(error)=>{
+app.listen(port, (error) => {
     if (!error){
-        console.log("Server Running on Port"+port)
+        console.log("Server Running on Port " + port);
+    } else {
+        console.log("Error: " + error);
     }
-    else{
-        console.log("Error :"+error)
-    }
-})
+});
