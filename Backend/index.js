a5bc6fbaa56926e608104cdcb487c43ecf8ff0a7
theprithwisingh <prithwisingh77@gmail.com>
2024-07-17 23:48:44 +0530

    schema

--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,7 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
+const { type } = require("os");
 
 app.use(express.json());
 app.use(cors());
@@ -35,7 +36,59 @@ app.post("/upload", upload.single('product'), (req, res) => {
         image_url: `http://localhost:${port}/image/${req.file.filename}`
     });
 });
+//schema for creating Products
+const Product = mongoose.model("Product",{
+    id:{
+        type:number,
+        required:true
+    },
+    name:{
+        type:string,
+        required:true,
+    },
+    image:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    new_price:{
+        type:Number,
+        required:true,
+    },
+    old_price:{
+        type:Number,
+        required:true,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    },
+    available:{
+        type:Boolean,
+        default:true,
+    },
+})
 
+app.post('/addproduct',async ()=>{
+        const product = new Product ({
+            id:req.body.id,
+            name:req.body.name,
+            image:req.body.image,
+            category:req.body.category,
+            new_price:req.body.new_price,
+            old_price:req.body.old_price,
+        });
+        console.log(product)
+        await product.save();
+        console.log("Saved");
+        res.json({
+            success:true,
+            name:req.body.name,
+        })
+})
 app.listen(port, (error) => {
     if (!error){
         console.log("Server Running on Port " + port);
